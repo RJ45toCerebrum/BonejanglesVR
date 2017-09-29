ac4807bece3fc064a417b034fc3dff2f4ce08961
Tyler Heers <tyler.heers@gmail.com>
2017-09-29 12:54:32 -0700

    Starting on snapping skeleton together

---
+++ BonejanglesVR/Assets/HangSkull.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class HangSkull : MonoBehaviour {
+    public Rigidbody rb;
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.name == "cranium") {
+            other.transform.position = transform.position;
+            SpringJoint sj = other.gameObject.AddComponent<SpringJoint>();
+            sj.autoConfigureConnectedAnchor = true;
+            sj.anchor = new Vector3(0, sj.anchor.y + 0.1f, 0);
+            sj.spring = 220;
+            sj.damper = 220;
+            sj.connectedBody = rb;
+        }
+    }
+}
