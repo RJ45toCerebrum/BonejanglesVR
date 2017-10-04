0ac46231390790efc66c6d47a8bfddb724ae85a6
Tyler Heers <tyler.heers@gmail.com>
2017-10-04 12:55:14 -0700

    Fixing skeleton issues

--- BonejanglesVR/Assets/Scripts/JointNode.cs
+++ BonejanglesVR/Assets/Scripts/JointNode.cs
@@ -1,12 +1,16 @@
 ﻿using VRTK;
 using UnityEngine;
+using System.Collections;
 
 namespace com.EvolveVR.BonejanglesVR
 {
     public class JointNode : MonoBehaviour
     {
+        private static VRTK_ControllerReference leftControllerReference;
+        private static VRTK_ControllerReference rightControllerReference;
         private VRTK_SnapDropZone snapDropZone;
         public string validObjectName;
+
 
         private void Awake()
         {
@@ -16,7 +20,21 @@ namespace com.EvolveVR.BonejanglesVR
                 snapDropZone.ObjectSnappedToDropZone += ObjectSnapped;
                 snapDropZone.ObjectUnsnappedFromDropZone += ObjectUnsnapped;
             }
+
+            StartCoroutine(LateStart());
         }
+
+        private IEnumerator LateStart()
+        {
+            yield return new WaitUntil(() =>
+            {
+                leftControllerReference = VRTK_ControllerReference.GetControllerReference(SDK_BaseController.ControllerHand.Left);
+                return leftControllerReference.hand == SDK_BaseController.ControllerHand.Left;
+            });
+
+            rightControllerReference = VRTK_ControllerReference.GetControllerReference(SDK_BaseController.ControllerHand.Right);
+        }
+
 
         private void ObjectUnsnapped(object sender, SnapDropZoneEventArgs e)
         {
@@ -31,26 +49,43 @@ namespace com.EvolveVR.BonejanglesVR
             // this is because only valid objects add connections
             if (e.snappedObject.name == validObjectName) {
                 GameManager gm = GameManager.Instance;
-                if(gm)
+                if (gm)
                     gm.RemoveConnection();
             }
+            else
+                StartCoroutine(PingPongPulses());
         }
 
         private void ObjectSnapped(object sender, SnapDropZoneEventArgs e)
         {
-            if (e.snappedObject.name != validObjectName)
+            if (e.snappedObject.name != validObjectName) {
                 snapDropZone.ForceUnsnap();
-            else
-            {
+            }
+            else {
                 Rigidbody otherRB = e.snappedObject.GetComponent<Rigidbody>();
-                otherRB.drag = 10.0f;
-                otherRB.angularDrag = 10.0f;
+                otherRB.drag = 12.0f;
+                otherRB.angularDrag = 20.0f;
 
                 GameManager gm = GameManager.Instance;
                 if (gm)
                     gm.AddConnection();
+
+                VRTK_ControllerHaptics.TriggerHapticPulse(leftControllerReference, 1.0f, 0.035f, 0.02f);
+                VRTK_ControllerHaptics.TriggerHapticPulse(rightControllerReference, 1.0f, 0.035f, 0.02f);
             }
 
+        }
+
+        private IEnumerator PingPongPulses()
+        {
+            for(int i = 0; i < 4; i++) {
+                if(i % 2 == 0)
+                    VRTK_ControllerHaptics.TriggerHapticPulse(leftControllerReference, 0.5f, 0.07f, 0.02f);
+                else
+                    VRTK_ControllerHaptics.TriggerHapticPulse(rightControllerReference, 0.5f, 0.07f, 0.02f);
+
+                yield return new WaitForSeconds(0.15f);
+            }  
         }
     }
 }
