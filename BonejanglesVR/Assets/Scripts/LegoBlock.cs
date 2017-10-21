1137009e6c8c5cf48ecc2805d39d2046a56d772d
Tyler Heers <tyler.heers@gmail.com>
2017-10-20 17:58:30 -0700

    Almost done with Alpha

--- BonejanglesVR/Assets/Scripts/LegoBlock.cs
+++ BonejanglesVR/Assets/Scripts/LegoBlock.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using VRTK;
 
@@ -6,23 +7,131 @@ namespace com.EvolveVR.BonejanglesVR
 {
     public class LegoBlock : MonoBehaviour
     {
+        private class ProngInfoList
+        {
+            public struct ProngInfo
+            {
+                public int prongIndex;
+                public Transform prongTransform;
+            }
+
+            public List<ProngInfo> otherProngsWithinRange;
+            public List<ProngInfo> myProngsInRange;
+
+            public ProngInfoList()
+            {
+                otherProngsWithinRange = new List<ProngInfo>();
+                myProngsInRange = new List<ProngInfo>();
+            }
+
+            /// <param name="index"> which index in the list does Transform of lego prong occur</param>
+            /// <param name="t">The prongs transform</param>
+            /// <param name="thisBlock">indicate whether this information is from this block or otherblock</param>
+            public void AddNew(int index, Transform t, bool thisBlock = true)
+            {
+                if (!Contains(t, thisBlock)) 
+                {
+                    ProngInfo pi = new ProngInfo();
+                    pi.prongIndex = index;
+                    pi.prongTransform = t;
+
+                    if (thisBlock)
+                        myProngsInRange.Add(pi);
+                    else
+                        otherProngsWithinRange.Add(pi);
+                }
+            }
+
+            private bool Contains(Transform t, bool thisBlock)
+            {
+                if (thisBlock) 
+                {
+                    return myProngsInRange.Any((p) => {
+                        return p.prongTransform == t;
+                    });
+                }
+
+                return otherProngsWithinRange.Any((p) => {
+                    return p.prongTransform == t;
+                });
+            }
+
+            public bool AreProngsAdjacent()
+            {
+                if (myProngsInRange.Count == minProngNumber)
+                    return AdjacencySnakeMethod() || AdjacencyLinearMethod();
+
+                return AdjacencySnakeMethod();
+            }
+
+            // uses the snake method to check if adjacent
+            private bool AdjacencySnakeMethod()
+            {
+                int lastNumber = myProngsInRange[0].prongIndex;
+                for(int i = 1; i < myProngsInRange.Count; i++)
+                {
+                    if ((myProngsInRange[i].prongIndex - lastNumber) == 1)
+                        lastNumber = myProngsInRange[i].prongIndex;
+                    else
+                        return false;
+                }
+
+                return true;
+            }
+
+            // uses the Linear method to check adjacency
+            private bool AdjacencyLinearMethod()
+            {
+                int lastNumber = myProngsInRange[0].prongIndex;
+                for (int i = 1; i < myProngsInRange.Count; i++) {
+                    if ((myProngsInRange[i].prongIndex - lastNumber) == 2)
+                        lastNumber = myProngsInRange[i].prongIndex;
+                    else
+                        return false;
+                }
+
+                return true;
+            }
+
+            public void Clear()
+            {
+                otherProngsWithinRange.Clear();
+                myProngsInRange.Clear();
+            }
+
+            public bool AllowedNumberOfProngs()
+            {
+                return myProngsInRange.Count == normalProngNumber ||
+                       myProngsInRange.Count == minProngNumber;
+            }
+
+            public List<Transform> ProngTransforms(bool thisBlock)
+            {
+                List<Transform> transforms = new List<Transform>();
+                if (thisBlock)
+                    myProngsInRange.ForEach((p) => { transforms.Add(p.prongTransform); });
+                else
+                    otherProngsWithinRange.ForEach((p) => { transforms.Add(p.prongTransform); });
+
+                return transforms;
+            }
+        }
+
         public bool debugMode = false;
         private VRTK_InteractableObject thisLegoInteractble;
 
         // dont forget about the weird trigger problem; this is why im doing this
         public GeneralTriggerEvent blockTriggerEvent;
-        private float maxProngDistance = 0.04f;
+        private float maxProngDistance = 0.035f;
         public const int normalProngNumber = 4;
         public const int minProngNumber = 2;
         
-
         public Transform[] topProngs;
         public Transform[] bottomProngs;
         public GameObject centroidPrimitive;
 
         private LegoBlock otherLegoBlock;
-        private List<Transform> otherProngsWithinRange;
-        private List<Transform> myProngsInRange;
+        private ProngInfoList prongInfoList;
 
         public bool isAnchored = false;
         public float breakForce = 500f;
@@ -57,8 +166,7 @@ namespace com.EvolveVR.BonejanglesVR
             blockTriggerEvent.SubOnTriggerStayEvent(OnBlockStay);
             blockTriggerEvent.SubOnTriggerExitEvent(OnBlockExit);
 
-            otherProngsWithinRange = new List<Transform>();
-            myProngsInRange = new List<Transform>();
+            prongInfoList = new ProngInfoList();
         }
 
 
@@ -71,30 +179,26 @@ namespace com.EvolveVR.BonejanglesVR
             if (otherLegoBlock == null || !isAnchored || otherLegoBlock.IsAnchored)
                 return;
 
-            otherProngsWithinRange.Clear();
-            myProngsInRange.Clear();
+            prongInfoList.Clear();
 
             Transform[] otherBottomProngs = otherLegoBlock.BottomProngs;
-            for(int i = 0; i < otherBottomProngs.Length; i++) 
+            for(int i = 0; i < topProngs.Length; i++) 
             {
-                Transform closestProngTransform = ClosestProng(topProngs[i].position, otherBottomProngs);
-                float distance = (topProngs[i].position - closestProngTransform.position).magnitude;
+                ProngInfoList.ProngInfo closestProng = ClosestProng(topProngs[i].position, otherBottomProngs);
+                float distance = (topProngs[i].position - closestProng.prongTransform.position).magnitude;
                 if (distance < maxProngDistance) 
                 {
-                    Debug.DrawLine(topProngs[i].position, closestProngTransform.position, Color.red, 0.1f);
-                    if (!otherProngsWithinRange.Contains(closestProngTransform)) {
-                        otherProngsWithinRange.Add(closestProngTransform);
-                        myProngsInRange.Add(topProngs[i]);
-                    }
+                    Debug.DrawLine(topProngs[i].position, closestProng.prongTransform.position, Color.red, 0.1f);
+                    prongInfoList.AddNew(i, topProngs[i], thisBlock: true);
+                    prongInfoList.AddNew(closestProng.prongIndex, closestProng.prongTransform, thisBlock: false);
                 }
             }
 
             // after we add all of the prongs within range; we need to see if they are of the correct numbers
-            if(otherProngsWithinRange.Count == normalProngNumber || otherProngsWithinRange.Count == minProngNumber)  
+            if(prongInfoList.AllowedNumberOfProngs())  
             {
-                if (AreProngsAdjacent(otherProngsWithinRange.Count)) {
+                if (prongInfoList.AreProngsAdjacent())
                     AnchorLego();
-                }
             }
         }
 
@@ -116,57 +220,10 @@ namespace com.EvolveVR.BonejanglesVR
             }
         }
 
-        private bool AreProngsAdjacent(int numProngs)
-        {
-            if(numProngs == minProngNumber)
-                return AdjacencySnakeMethod(numProngs) || AdjacencyLinearMethod();
-
-            return AdjacencySnakeMethod(numProngs);
+        private void OnJointBreak(float breakForce){
+            isAnchored = false;
         }
 
-        // uses the snake method to check if adjacent
-        private bool AdjacencySnakeMethod(int numProngs)
-        {
-            // snake method
-            bool firstProngEncountered = false;
-            int j = 0;
-            for (int i = 0; j < numProngs; i++) {
-                if (topProngs[i] == myProngsInRange[j]) {
-                    firstProngEncountered = true;
-                    j++;
-                }
-                else if (firstProngEncountered)
-                    return false;
-            }
-
-            return true;
-        }
-
-        // uses the Linear method to check adjacency
-        private bool AdjacencyLinearMethod()
-        {
-            // topProngs and myProngsWithinRange
-            bool firstProngEncounteredLeft = false;
-            bool firstProngEncounteredRight = false;
-            int j = 0;
-
-            // check the left top prongs; i --> 0, 2, 4, 6  k --> 1, 3, 5, 7
-            for (int i = 0, k = 1; (j < 2 && k < topProngs.Length && i < topProngs.Length); i+=2, k+=2)
-            {
-                if (topProngs[i] == myProngsInRange[j] && !firstProngEncounteredRight) {
-                    firstProngEncounteredLeft = true;
-                    j++;
-                }
-                else if (topProngs[k] == myProngsInRange[j] && !firstProngEncounteredLeft) {
-                    firstProngEncounteredRight = true;
-                    j++;
-                }
-                else if (firstProngEncounteredLeft || firstProngEncounteredRight)
-                    return false;
-            }
-
-            return true;
-        }
 
         private Vector3 CreateCentroid(List<Transform> ts)
         {
@@ -177,24 +234,29 @@ namespace com.EvolveVR.BonejanglesVR
                 centroid.z += t.position.z;
             }
 
-            centroid /= otherProngsWithinRange.Count;
+            centroid /= ts.Count;
             return centroid;
         }
 
-        private Transform ClosestProng(Vector3 position, Transform[] otherBottomProngs)
+        private ProngInfoList.ProngInfo ClosestProng(Vector3 position, Transform[] otherBottomProngs)
         {
+            int index = 0;
             Transform closestTransform = otherBottomProngs[0];
             float closestProngDistance = Vector3.Distance(position, otherBottomProngs[0].position);
 
-            for (int r = 0; r < otherBottomProngs.Length; r++) {
+            for (int r = 1; r < otherBottomProngs.Length; r++) {
                 float distance = Vector3.Distance(position, otherBottomProngs[r].position);
                 if (distance < closestProngDistance) {
                     closestProngDistance = distance;
                     closestTransform = otherBottomProngs[r];
+                    index = r;
                 }
             }
 
-            return closestTransform;
+            ProngInfoList.ProngInfo info = new ProngInfoList.ProngInfo();
+            info.prongIndex = index;
+            info.prongTransform = closestTransform;
+            return info;
         }
 
         private void AnchorLego()
@@ -206,8 +268,8 @@ namespace com.EvolveVR.BonejanglesVR
             // orient the lego and put it into place; a create a empty game object a proper position
             // and move that into place and then destroy it
             OrientTransform(otherLegoBlock.transform);
-            Vector3 otherProngsCentroid = CreateCentroid(otherProngsWithinRange);
-            Vector3 myProngsCentroid = CreateCentroid(myProngsInRange);
+            Vector3 otherProngsCentroid = CreateCentroid(prongInfoList.ProngTransforms(thisBlock : false));
+            Vector3 myProngsCentroid = CreateCentroid(prongInfoList.ProngTransforms(thisBlock : true));
             GameObject otherCentroidGO = GameObject.Instantiate(centroidPrimitive);
             otherCentroidGO.transform.position = otherProngsCentroid;
             otherCentroidGO.transform.rotation = otherLegoBlock.transform.rotation;
@@ -275,10 +337,6 @@ namespace com.EvolveVR.BonejanglesVR
                 return -(Mathf.Abs(v) - remainder);
             else
                 return v + multiple - remainder;
-        }
-
-        private void OnJointBreak(float breakForce) {
-            isAnchored = false;
         }
     }
 }
